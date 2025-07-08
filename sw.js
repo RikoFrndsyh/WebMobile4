d8e88353d222a1405ccd477b49995f71c503d2c8
RikoFrndsyh <139275643+RikoFrndsyh@users.noreply.github.com>
2025-07-08 19:42:56 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -14,8 +14,8 @@ self.addEventListener("install", event => {
       "./berita-api.js",
       "./manifest.json",
       "./offline.html",
-      "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css",
-      "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+      "https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css",
+      "https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
     ]);
     } catch (err) {
       console.error("Gagal menyimpan cache:", err);
